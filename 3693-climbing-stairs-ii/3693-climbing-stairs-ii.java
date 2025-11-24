2ddd93298f703f69670bce66655b9c8382ac909d
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2025-11-23 17:53:04 -0800

    8 ms (79.32%) 3693-climbing-stairs-ii Medium .java

--- 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
+++ 3693-climbing-stairs-ii/3693-climbing-stairs-ii.java
@@ -8,13 +8,13 @@ class Solution {
         for (int i = 1; i <= n; i++) {
             int step1 = Integer.MAX_VALUE, step2 = Integer.MAX_VALUE, step3 = Integer.MAX_VALUE;
             if (i - 1 >= 0) {
-                step1 = dp[i-1] + costs[i-1] + (int) Math.pow(i-(i-1), 2);
+                step1 = dp[i-1] + costs[i-1] + 1;
             }
             if (i - 2 >= 0) {
-                step2 = dp[i-2] + costs[i-1] + (int) Math.pow(i-(i-2), 2);
+                step2 = dp[i-2] + costs[i-1] + 4;
             }
             if (i - 3 >= 0) {
-                step3 = dp[i-3] + costs[i-1] + (int) Math.pow(i-(i-3), 2);
+                step3 = dp[i-3] + costs[i-1] + 9;
             }
             dp[i] = step1 < step2 && step1 < step3 ? step1 : step2 < step3 ? step2 : step3;
         }
