0db4b296c9e1d8297d25b1424a5ba1ba6fb159ee
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2025-11-25 18:56:45 -0800

    15 ms (5.48%) 1015-smallest-integer-divisible-by-k Medium .java

---
+++ 1015-smallest-integer-divisible-by-k/1015-smallest-integer-divisible-by-k.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int smallestRepunitDivByK(int k) {
+        if (k % 2 == 0) {
+            return -1;
+        }
+        Set<Integer> remSet = new HashSet<>();
+        int currRem = 1;
+        List<Integer> list = new ArrayList<>();
+        while (true) {
+            list.add(1);
+            currRem = currRem % k;
+            if (currRem == 0) {
+                break;
+            } else {
+                if (remSet.contains(currRem)) {
+                    return -1;
+                }
+                remSet.add(currRem);
+            }
+            currRem = currRem * 10 + 1;
+        }
+        return list.size();
+    }
+}
