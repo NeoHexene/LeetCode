ae9605fdad72c89c50c1cf9220e037445f82d849
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2025-12-19 16:54:06 -0800

    55 ms (74.93%) 1470-shuffle-the-array Easy .py

--- 1470-shuffle-the-array/1470-shuffle-the-array.py
+++ 1470-shuffle-the-array/1470-shuffle-the-array.py
@@ -1,15 +1,11 @@
 class Solution:
     def shuffle(self, nums: List[int], n: int) -> List[int]:
         ans = []
-        midIdx = n
         startIdx = 0
-        temp = n
 
-        while temp > 0:
+        while startIdx < n:
             ans.append(nums[startIdx])
-            ans.append(nums[midIdx])
-            temp -= 1
+            ans.append(nums[startIdx + n])
             startIdx += 1
-            midIdx += 1
 
         return ans
