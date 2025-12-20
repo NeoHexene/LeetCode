1e6b45fb7cd4bc6595e0c630861c856e243158c2
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2025-12-19 16:53:39 -0800

    0 ms (100%) 1929-concatenation-of-array Easy .py

--- 1929-concatenation-of-array/1929-concatenation-of-array.py
+++ 1929-concatenation-of-array/1929-concatenation-of-array.py
@@ -1,13 +1,6 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        ans = []
-        times = 2
 
-        while times > 0:
-            for n in nums:
-                ans.append(n)
-            times -= 1
-
-        return ans
+        return nums+nums
 
         
