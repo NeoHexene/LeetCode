0ca4f7227d7f3023ea241f49b8e088499bfb46c6
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2025-12-19 16:54:29 -0800

    3 ms (95.59%) 0645-set-mismatch Easy .py

---
+++ 0645-set-mismatch/0645-set-mismatch.py
@@ -0,0 +1,8 @@
+class Solution:
+    def findErrorNums(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        total = int(n * (n + 1) / 2)
+        missing = total - sum(set(nums))
+        duplicate = sum(nums) - (total - missing)
+        return [duplicate, missing]
+        
