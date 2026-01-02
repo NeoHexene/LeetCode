4a030d77d228387c7adb90b4f68fe6face2fca0e
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-01-01 23:40:10 -0800

    0 ms (100%) 0961-n-repeated-element-in-size-2n-array Easy .java

---
+++ 0961-n-repeated-element-in-size-2n-array/0961-n-repeated-element-in-size-2n-array.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int repeatedNTimes(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (Integer n : nums) {
+            if (set.contains(n)) {
+                return n;
+            }
+            set.add(n);
+        }
+        return -1;
+    }
+}
