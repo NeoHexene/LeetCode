d5a1cdc24bbde46c7c21e6f5ed1fa11259aca38b
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-03-07 20:05:53 -0800

    157 ms (5.04%) 3863-minimum-operations-to-sort-a-string Medium .java

---
+++ 3863-minimum-operations-to-sort-a-string/3863-minimum-operations-to-sort-a-string.java
@@ -0,0 +1,62 @@
+class Solution {
+    public int minOperations(String s) {
+        if (s.length() == 1) {
+            return 0;
+        }
+        if (s.length() == 2) {
+            if (s.charAt(0) > s.charAt(1)) {
+                return -1;
+            }
+            return 0;
+        }
+        int idx = sortCheck(s);
+        if (idx != -1) {
+            int backCount = looping (s, false);
+            int frontCount = looping (s, true);
+            return Math.min(backCount, frontCount);
+        }
+        return 0;
+    }
+
+    private String backSorted (String s) {
+        char[] backArray = s.substring(1, s.length()).toCharArray();
+        Arrays.sort(backArray);
+        return s.charAt(0) + String.valueOf(backArray);
+    }
+
+    private String frontSorted (String s) {
+        char[] frontArray = s.substring(0, s.length() - 1).toCharArray();
+        Arrays.sort(frontArray);
+        return String.valueOf(frontArray) + s.charAt(s.length() - 1);
+    }
+
+    private int sortCheck (String s) {
+        boolean sortReq = false;
+        int idx = -1;
+        for (int i = 1; i < s.length() && !sortReq; i++) {
+            if (!(s.charAt(i - 1) <= s.charAt(i))) {
+                idx = i;
+                sortReq = true;
+            }
+        }
+        return idx;
+    }
+
+    private int looping (String s, boolean front) {
+        int count = 0;
+        while (sortCheck(s) != -1) {
+            if (!front) {
+                s = backSorted(s);
+            } else {
+                s = frontSorted(s);
+            }
+            front = !front;
+            count += 1;
+            System.out.println("S: "+s);
+        }
+        return count;
+    }
+}
+
+// 4: 1
+// 5: 2
