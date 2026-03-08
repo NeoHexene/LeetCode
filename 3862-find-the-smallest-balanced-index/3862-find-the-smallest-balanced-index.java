2be38e93a66b5eb8a6760407908b8b5f75455c8d
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-03-07 20:03:27 -0800

    6 ms (78.82%) 3862-find-the-smallest-balanced-index Medium .java

---
+++ 3862-find-the-smallest-balanced-index/3862-find-the-smallest-balanced-index.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int smallestBalancedIndex(int[] nums) {
+        long[] presum = new long[nums.length + 1];
+        presum[0] = 0;
+        for (int i = 0; i < nums.length; i++) {
+            presum[i + 1] = presum[i] + nums[i];
+        }
+        long[] sufprod = new long[nums.length + 1];
+        sufprod[nums.length] = 1;
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (sufprod[i+1] > presum[nums.length]) {
+                sufprod[i] = sufprod[i + 1];
+            } else {
+                sufprod[i] = sufprod[i + 1] * nums[i];
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (presum[i] == sufprod[i+1]) {
+                return i;
+            }
+        }
+        return -1;
+    }
+}
