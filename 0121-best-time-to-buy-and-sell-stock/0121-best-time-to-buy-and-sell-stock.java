3ff2f8cea6e86afd69bdda6df31149323ea0ef7f
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-02 16:15:22 -0700

    N/A (0%) 0121-best-time-to-buy-and-sell-stock Easy .java

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,20 +1,34 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        
+        // If the array is empty, no transactions can be made
         if (prices.length < 1) {
             return 0;
         }
-        
+
+        // This will store the maximum profit we can achieve
         int maxProfit = 0;
+
+        // Assume we buy on the first day initially
         int buy = prices[0];
-        
+
+        // Start checking from day 1 (since day 0 is already considered as buy)
         for (int i = 1; i < prices.length; i++) {
+
+            // If we find a lower price than our current buying price,
+            // update buy to this new lower price (better buying opportunity)
             if (buy > prices[i]) {
                 buy = prices[i];
             } else {
-                maxProfit = Math.max(maxProfit,prices[i] - buy);
+                // Otherwise, calculate profit if we sell today
+                // profit = current price - buying price
+
+                // Update maxProfit if this profit is higher than previous ones
+                maxProfit = Math.max(maxProfit, prices[i] - buy);
             }
         }
-        
+
+        // Return the maximum profit found
         return maxProfit;
     }
 }
