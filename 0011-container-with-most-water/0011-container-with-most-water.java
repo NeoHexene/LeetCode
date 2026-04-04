cec69f78b51015a881aa353e9f894220fa8b3509
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-04 13:52:50 -0700

    5 ms (82.98%) 0011-container-with-most-water Medium .java

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -1,16 +1,25 @@
 class Solution {
+    
+    /*
+    [1, 8, 6, 2, 5, 4, 8, 3, 7]
+    area = length * breadth
+    keep on moving the side whose height is less
+
+    [1, 1]
+    */
+
     public int maxArea(int[] height) {
         int left = 0, right = height.length - 1;
         int max = 0;
         while (left < right) {
             int length = right - left;
-            int breadth = height[left] < height[right] ? height[left] : height[right];
+            int breadth = Math.min(height[right], height[left]);
             max = Math.max(max, length * breadth);
-            if (height[left] < height[right]) {
-                left ++;
+            if (height[left] > height[right]) {
+                right--;
             } else {
-                right --;
-            }    
+                left++;
+            }
         }
         return max;
     }
