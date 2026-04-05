fac58703171a1ca7e65a1ac5de87892ca47b63ed
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-04 19:41:31 -0700

    N/A (0%) 0007-reverse-integer Medium .java

--- 0007-reverse-integer/0007-reverse-integer.java
+++ 0007-reverse-integer/0007-reverse-integer.java
@@ -1,20 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        int n = Math.abs(x);
         long ans = 0;
-        while(n > 0) {
-            System.out.println("Ans: "+ans+" n: "+n);
-            int rem = n % 10;
-            ans *= 10;
-            ans += rem;
-            n = n / 10;
+        while (x != 0) {
+            long temp = x % 10;
+            ans = ans * 10 + temp;
+            x = x / 10;
         }
-
-        if (ans > Integer.MAX_VALUE) {
+        if (ans < Integer.MIN_VALUE || ans > Integer.MAX_VALUE) {
             return 0;
-        } else {
-            int res = (int) ans * (x < 0 ? -1 : 1);
-            return res;
         }
+        return (int) ans;
     }
 }
