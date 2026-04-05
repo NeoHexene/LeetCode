21c44f677e9d0db5836c1afcc473f6a2b2eaec53
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-04 19:04:37 -0700

    N/A (0%) 0056-merge-intervals Medium .java

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,18 +1,21 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
-        List<int[]> merged = new ArrayList<>();
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> mergeList = new ArrayList<>();
         int[] prev = intervals[0];
         for (int i = 1; i < intervals.length; i++) {
-            int interval[] = intervals[i];
-            if (interval[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], interval[1]);
+            int[] curr = intervals[i];
+            if (prev[1] >= curr[0]) {
+                curr[0] = prev[0];
+                curr[1] = Math.max(prev[1], curr[1]);
+                prev = curr;
             } else {
-                merged.add(prev);
-                prev = interval;
+                mergeList.add(prev);
+                prev = curr;
             }
         }
-        merged.add(prev);
-        return merged.toArray(new int[merged.size()][]);
+        mergeList.add(prev);
+        return mergeList.toArray(new int[mergeList.size()][2]);
     }
 }
