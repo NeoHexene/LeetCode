c9ee313b523d08556c6a1af709654c131edae1a5
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-05 16:32:11 -0700

    0 ms (100%) 0021-merge-two-sorted-lists Easy .java

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -10,63 +10,20 @@
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-        ListNode head = null;
-        ListNode currNode = null;
-        ListNode prevNode = null;
-        ListNode curr1 = list1;
-        ListNode curr2 = list2;
-        while (curr1 != null && curr2 != null) {
-            if (curr1.val < curr2.val) {
-                if (head == null) {
-                    head = curr1;
-                }
-                prevNode = currNode;
-                currNode = curr1;
-                if (prevNode != null) {
-                    prevNode.next = currNode;
-                }
-                curr1 = curr1.next;
+        ListNode head = new ListNode();
+        ListNode curr = head;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                curr.next = list1;
+                list1 = list1.next;
             } else {
-                if (head == null) {
-                    head = curr2;
-                    
-                }
-                prevNode = currNode;
-                currNode = curr2;
-                if (prevNode != null) {
-                    prevNode.next = currNode;
-                }
-                curr2 = curr2.next;
+                curr.next = list2;
+                list2 = list2.next;
             }
+            curr = curr.next;
         }
-        if (curr1 != null) {
-            while (curr1 != null) {
-                if (head == null) {
-                    head = curr1;
-                    
-                }
-                prevNode = currNode;
-                currNode = curr1;
-                if (prevNode != null) {
-                    prevNode.next = currNode;
-                }
-                curr1 = curr1.next;
-            }
-        }
-        if (curr2 != null) {
-            while (curr2 != null) {
-                if (head == null) {
-                    head = curr2;
-                    
-                }
-                prevNode = currNode;
-                currNode = curr2;
-                if (prevNode != null) {
-                    prevNode.next = currNode;
-                }
-                curr2 = curr2.next;
-            }
-        }
-        return head;
+        curr.next = list1 != null ? list1 : list2;
+        return head.next;
     }
 }
