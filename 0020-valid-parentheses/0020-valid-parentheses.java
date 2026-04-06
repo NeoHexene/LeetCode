8087739c4f31f9e96b83c569363129c07b49b642
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-05 17:11:05 -0700

    4 ms (41.19%) 0020-valid-parentheses Easy .java

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else if (!st.isEmpty() && ((c == ')' && st.peek() != '(') || (c == ']' && st.peek() != '[') || (c == '}' && st.peek() != '{'))) {
+                return false;
+            } else if (st.isEmpty() && (c == ')' || c == '}' || c == ']')) {
+                return false;
+            } else {
+                st.pop();
+            }
+        }
+        return st.isEmpty() ? true : false;
+    }
+}
