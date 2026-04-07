3604731f9015604e5ccf8aa8dd6f9cb247b658a3
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-07 13:27:59 -0700

    N/A (0%) 0033-search-in-rotated-sorted-array Medium .java

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,25 +1,16 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int l = 0;
-        int r = nums.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (nums[m] == target) {
-                return m;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                return mid;
             }
-            if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target <= nums[m]) {
-                    r = m - 1;
+            if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
                 } else {
-                    l = m + 1;
+                    left = mid + 1;
                 }
-            } else {
-                if (nums[m] <= target && target <= nums[r]) {
-                    l = m + 1;
-                } else {
-                    r = m - 1;
-                }
-            }
         }
         return -1;
     }
