a36c3c564313f0d69e608a36f2b838966857c04f
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-07 13:30:41 -0700

    0 ms (100%) 0033-search-in-rotated-sorted-array Medium .java

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -6,11 +6,19 @@ class Solution {
             if (nums[mid] == target) {
                 return mid;
             }
-            if (nums[left] <= target && target < nums[mid]) {
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
                     right = mid - 1;
                 } else {
                     left = mid + 1;
                 }
+            } else {
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
+            }
         }
         return -1;
     }
