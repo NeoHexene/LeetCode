4625f4274186bf2f33b6e2c205d2ff4005f29ad3
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-04-17 18:14:25 -0700

    N/A (0%) 0654-maximum-binary-tree Medium .java

--- 0654-maximum-binary-tree/0654-maximum-binary-tree.java
+++ 0654-maximum-binary-tree/0654-maximum-binary-tree.java
@@ -14,7 +14,9 @@
  * }
  */
 class Solution {
+    
     public TreeNode constructMaximumBinaryTree(int[] nums) {
+        public TreeNode constructMaximumBinaryTree(int[] nums) {
         return dfs(nums);
     }
 
