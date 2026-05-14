bcb6cd4eb48b2ba4e6c60e08482dd9296b76fc22
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-05-13 18:26:03 -0700

    2 ms (91.7%) 0283-move-zeroes Easy .java

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -54,16 +54,21 @@ Two pointers (slow + fast) for in-place array rearrangement
 class Solution {
     public void moveZeroes(int[] nums) {
 
-        int i = 0; // position to place next non-zero
+        int i = -1;
 
         for (int j = 0; j < nums.length; j++) {
-
+            if (nums[j] == 0) {
+                i = j;
+                break;
+            }
+        }
+        if (i == -1) {
+            return;
+        }
+        for (int j = i + 1; j < nums.length; j++) {
             if (nums[j] != 0) {
-                // swap nums[i] and nums[j]
-                int temp = nums[i];
                 nums[i] = nums[j];
-                nums[j] = temp;
-
+                nums[j] = 0;
                 i++;
             }
         }
