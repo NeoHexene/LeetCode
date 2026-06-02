ad7cad155969f16e6af9598b9efa7f29b6d6de21
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-01 21:14:29 -0700

    128 ms (5.07%) 3121-count-the-number-of-special-characters-ii Medium .java

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.java
@@ -1,23 +1,31 @@
 class Solution {
     public int numberOfSpecialChars(String word) {
-        Map<Character, Integer> lastOccurrenceOfLowercase = new HashMap<>();
+        int[] lc = new int[26];
+        int[] uc = new int[26];
+
+        for (int i = 0; i < 26; i++) {
+            lc[i] = -1;
+            uc[i] = -1;
+        }
+
+
         for (int i = 0; i < word.length(); i++) {
             char c = word.charAt(i);
             if (Character.isLowerCase(c)) {
-                lastOccurrenceOfLowercase.put(c, i);
+                lc[c - 'a'] = i;
+            } else if (uc[c - 'A'] == -1) {
+                uc[c - 'A'] = i;
             }
         }
+        
         int count = 0;
-        for (int i = 0; i < word.length(); i++) {
-            char c = word.charAt(i);
-            char sc = Character.toLowerCase(c);
-            if (Character.isUpperCase(c) && lastOccurrenceOfLowercase.containsKey(sc)) {
-                if (i > lastOccurrenceOfLowercase.get(sc)) {
-                    count += 1;
-                }
-                lastOccurrenceOfLowercase.remove(sc);
+
+        for (int i = 0; i < 26; i++) {
+            if (lc[i] != -1 && uc[i] != -1 && lc[i] < uc[i]) {
+                count += 1;
             }
         }
+
         return count;
     }
 }
