f82932ac586d562bd534f247afbf351519d4268b
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-05 09:40:20 -0700

    23 ms (93.71%) 0560-subarray-sum-equals-k Medium .java

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.java
@@ -1,64 +1,24 @@
 class Solution {
-
-    /*
-    INTUITION:
-
-    We want to count subarrays with sum = k.
-
-    Instead of checking all subarrays (slow), we use a trick:
-
-    At every index, we keep a running sum = sum.
-
-    Now we ask:
-    "Have I seen a previous sum such that removing it gives k?"
-
-    That means:
-        currentSum - previousSum = k
-        previousSum = currentSum - k
-
-    So:
-       If (sum - k) has appeared before,
-       then we found a subarray ending here with sum = k.
-
-    We use a HashMap to store:
-        prefixSum -> how many times we have seen it
-
-    Why frequency?
-    Because the same prefix sum can appear multiple times,
-    and each occurrence gives a valid subarray.
-
-    KEY MEMORY TRICK:
-       "Check (sum - k), then store sum"
-    */
-
     public int subarraySum(int[] nums, int k) {
 
-        // Map: prefix sum -> frequency
-        Map<Integer, Integer> prefSum = new HashMap<>();
+        Map<Integer, Integer> prefixSum = new HashMap<>();
 
-        // Important:
-        // Before starting, sum = 0 has occurred once
-        // Helps count subarrays starting from index 0
-        prefSum.put(0, 1);
+        int sum = 0;
+        int count = 0;
 
-        int sum = 0;   // running sum
-        int count = 0; // answer
+        prefixSum.put(0, 1);
 
-        for (int num : nums) {
+        for (int n : nums) {
+            sum += n;
 
-            // Update running sum
-            sum += num;
-
-            // Check if we have seen (sum - k) before
-            // If yes, those many subarrays end here with sum = k
-            if (prefSum.containsKey(sum - k)) {
-                count += prefSum.get(sum - k);
+            if (prefixSum.containsKey(sum - k)) {
+                count += prefixSum.get(sum - k); 
             }
 
-            // Store current sum for future use
-            prefSum.put(sum, prefSum.getOrDefault(sum, 0) + 1);
+            prefixSum.put(sum, prefixSum.getOrDefault(sum, 0) + 1);
         }
 
         return count;
+
     }
 }
