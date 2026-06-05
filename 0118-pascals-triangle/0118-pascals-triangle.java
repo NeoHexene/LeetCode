2eaca6b285c36b72242b19462023da35497dd29b
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-05 16:12:24 -0700

    1 ms (94.47%) 0118-pascals-triangle Easy .java

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,25 +1,17 @@
 class Solution {
     public List<List<Integer>> generate(int numRows) {
         List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < numRows; i++) {
-            res.add(new ArrayList<>());
-            if (i == 0) {
-                res.get(i).add(1);
-            }
-            if (i == 1) {
-                res.get(i).add(1);
-                res.get(i).add(1);
-            }
-        }
 
-        for (int curr = 2; curr < numRows; curr++) {
+        for (int curr = 0; curr < numRows; curr++) {
+            List<Integer> temp = new ArrayList<>();
             for (int i = 0; i < curr + 1; i++) {
                 if (i == 0 || i == curr) {
-                    res.get(curr).add(1);
-                    continue;
+                    temp.add(1);
+                } else {
+                    temp.add(res.get(curr - 1).get(i - 1) + res.get(curr - 1).get(i));
                 }
-                res.get(curr).add(res.get(curr - 1).get(i - 1) + res.get(curr - 1).get(i));
             }
+            res.add(temp);
         }
 
         return res;
