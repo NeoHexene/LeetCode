c0bf708cc7e9cb2affe7bd3ebde6c6a07cd1c06d
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-05 21:39:28 -0700

    2081 ms (5%) 0015-3sum Medium .java

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,71 +1,24 @@
 class Solution {
-
-    /*
-    INTUITION:
-    We need to find all unique triplets such that:
-        nums[i] + nums[j] + nums[k] == 0
-
-    Brute force checks every triplet, which is too slow: O(n^3).
-
-    Better idea:
-    1. Sort the array.
-    2. Fix one number nums[i].
-    3. Now the problem becomes finding two numbers in the बाकी part
-       such that their sum is -nums[i].
-
-    Since the array is sorted:
-    - If the sum is too small, move the left pointer forward.
-    - If the sum is too big, move the right pointer backward.
-
-    To avoid duplicate triplets:
-    - Skip repeated values of nums[i]
-    - After finding a valid triplet, skip repeated left/right values too
-    */
-
     public List<List<Integer>> threeSum(int[] nums) {
+
         List<List<Integer>> res = new ArrayList<>();
+        Set<String> check = new HashSet<>();
 
-        // Sort first so we can use two pointers
-        Arrays.sort(nums);
-
-        for (int i = 0; i < nums.length - 2; i++) {
-
-            // Skip duplicate first numbers
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
-
-            int j = i + 1;
-            int k = nums.length - 1;
-
-            while (j < k) {
-
-                int sum = nums[i] + nums[j] + nums[k];
-
-                if (sum == 0) {
-                    // Found one valid triplet
-                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
-
-                    // Move both pointers
-                    j++;
-                    k--;
-
-                    // Skip duplicate values on the left
-                    while (j < k && nums[j] == nums[j - 1]) {
-                        j++;
+        for (int i = 0; i < nums.length; i++) {
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < nums.length; j++) {
+                if (map.containsKey(0 - nums[i] - nums[j])) {
+                    int k = map.get(0 - nums[i] - nums[j]);
+                    int[] curr = new int[]{nums[i], nums[j], nums[k]};
+                    Arrays.sort(curr);
+                    String s = curr[0] + " " + curr[1] + " " + curr[2];
+                    if (!check.contains(s)) {
+                        check.add(s);
+                        List<Integer> temp = List.of(nums[i], nums[j], nums[k]);
+                        res.add(temp);
                     }
-
-                    // Skip duplicate values on the right
-                    while (j < k && nums[k] == nums[k + 1]) {
-                        k--;
-                    }
-
-                } else if (sum < 0) {
-                    // Sum is too small, need a bigger number
-                    j++;
                 } else {
-                    // Sum is too large, need a smaller number
-                    k--;
+                    map.put(nums[j], j);
                 }
             }
         }
