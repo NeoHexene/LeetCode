de020b592cee9b6038f0a185abed72212acb2ef1
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-05 21:41:38 -0700

    35 ms (42.73%) 0015-3sum Medium .java

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
         List<List<Integer>> res = new ArrayList<>();
-        Set<String> check = new HashSet<>();
 
-        for (int i = 0; i < nums.length; i++) {
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int j = i + 1; j < nums.length; j++) {
-                if (map.containsKey(0 - nums[i] - nums[j])) {
-                    int k = map.get(0 - nums[i] - nums[j]);
-                    int[] curr = new int[]{nums[i], nums[j], nums[k]};
-                    Arrays.sort(curr);
-                    String s = curr[0] + " " + curr[1] + " " + curr[2];
-                    if (!check.contains(s)) {
-                        check.add(s);
-                        List<Integer> temp = List.of(nums[i], nums[j], nums[k]);
-                        res.add(temp);
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int j = i + 1;
+            int k = nums.length - 1;
+
+            while (j < k) {
+
+                int sum = nums[i] + nums[j] + nums[k];
+
+                if (sum == 0) {
+                    
+                    res.add(Arrays.asList(nums[i], nums[j], nums[k]));
+
+                    j++;
+                    k--;
+
+                    while (j < k && nums[j] == nums[j - 1]) {
+                        j++;
                     }
+
+                    while (j < k && nums[k] == nums[k + 1]) {
+                        k--;
+                    }
+
+                } else if (sum < 0) {
+                    
+                    j++;
                 } else {
-                    map.put(nums[j], j);
+                    
+                    k--;
                 }
             }
         }
