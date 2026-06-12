f1344cea5afbdf0d86827fd29ec1c28267b17098
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-12 12:43:40 -0700

    1 ms (100%) 3954-sum-of-compatible-numbers-in-range-i Easy .java

--- 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.java
+++ 3954-sum-of-compatible-numbers-in-range-i/3954-sum-of-compatible-numbers-in-range-i.java
@@ -4,14 +4,14 @@ abs(n - x) <= k
 n & x == 0
 
 
-range of x would be 1 to n + 2 * k
+range of x would be 1 to n + k
 */
 class Solution {
     public int sumOfGoodIntegers(int n, int k) {
         int res = 0;
-        for (int start = 1; start <= n + k; start++) {
-            if((n & start) == 0 && Math.abs(n - start) <= k) {
-                res += start;
+        for (int x = 1; x <= n + k; x++) {
+            if((n & x) == 0 && Math.abs(n - x) <= k) {
+                res += x;
             }
         }
         return res;
