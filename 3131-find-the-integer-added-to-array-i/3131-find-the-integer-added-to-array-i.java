544b68b49474bafd806abed5547eafb7364a0434
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-16 22:50:19 -0700

    0 ms (100%) 3131-find-the-integer-added-to-array-i Easy .java

--- 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
+++ 3131-find-the-integer-added-to-array-i/3131-find-the-integer-added-to-array-i.java
@@ -1,7 +1,12 @@
 class Solution {
     public int addedInteger(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
-        return nums2[0] - nums1[0];
+        int max1 = 0, max2 = 0;
+
+        for (int i = 0; i < nums1.length; i++) {
+            max1 = Math.max(max1, nums1[i]);
+            max2 = Math.max(max2, nums2[i]);
+        }
+
+        return max2 - max1;
     }
 }
