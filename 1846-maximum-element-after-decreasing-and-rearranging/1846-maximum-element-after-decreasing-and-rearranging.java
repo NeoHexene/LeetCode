859d47369d5b28a1e0957cc1ac14e49ec3534e7c
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-06-27 23:32:52 -0700

    8 ms (82.92%) 1846-maximum-element-after-decreasing-and-rearranging Medium .java

--- 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
+++ 1846-maximum-element-after-decreasing-and-rearranging/1846-maximum-element-after-decreasing-and-rearranging.java
@@ -1,24 +1,13 @@
 class Solution {
     public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
-        if (arr[0] != 1) {
-            Arrays.sort(arr);
-        }
+        Arrays.sort(arr);
 
-        if (arr[0] != 1) {
-            arr[0] = 1;
-        }
-
-        int n = 2;
         int max = 1;
 
         for (int i = 1; i < arr.length; i++) {
-            if (!(Math.abs(arr[i] - arr[i - 1]) <= 1)) {
-                arr[i] = n;
-                n++;
-            } else {
-                n = arr[i] + 1;
+            if (arr[i] > max) {
+                max++;
             }
-            max = Math.max(max, arr[i]);
         }
 
         return max;
