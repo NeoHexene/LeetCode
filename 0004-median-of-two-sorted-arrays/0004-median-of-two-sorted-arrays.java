17504cc615c1cb65bea78e19f905f1a58b12ea76
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-07-08 00:46:23 -0700

    1 ms (100%) 0004-median-of-two-sorted-arrays Hard .java

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,45 +1,39 @@
 class Solution {
-
-    static {
-    Runtime.getRuntime().addShutdownHook(new Thread(() -> {
-        try (java.io.FileWriter fw = new java.io.FileWriter("display_runtime.txt")) {
-            fw.write("0");
-        } catch (Exception e) {
-        }
-    }));
-    }
-    
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        int m = nums1.length;
-        int n = nums2.length;
-        int[] merge = new int[m+n];
-        int i = 0, j = 0, k = 0;
-        while (i < m && j < n) {
-            if(nums1[i] > nums2[j]) {
-                merge[k] = nums2[j];
-                k++;
-                j++;
+
+        int n1 = nums1.length;
+        int n2 = nums2.length;
+
+        if (n2 < n1) {
+            return findMedianSortedArrays(nums2, nums1);
+        }
+
+        int low = 0, high = n1;
+
+        while (low <= high) {
+            int cut1 = low + (high - low) / 2;
+            int cut2 = (n1 + n2 + 1) / 2 - cut1;
+
+            int l1 = cut1 == 0 ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int l2 = cut2 == 0 ? Integer.MIN_VALUE : nums2[cut2 - 1];
+
+            int r1 = cut1 == n1 ? Integer.MAX_VALUE : nums1[cut1];
+            int r2 = cut2 == n2 ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (l1 <= r2 && l2 <= r1) {
+                if ((n1 + n2) % 2 == 0) {
+                    return ((Math.max(l1, l2) + Math.min(r1, r2)) / 2.0);
+                } else {
+                    return Math.max(l1, l2);
+                }
+            }
+
+            if (l1 > r2) {
+                high = cut1 - 1;
             } else {
-                merge[k] = nums1[i];
-                k++;
-                i++;
+                low = cut1 + 1;
             }
         }
-        while (i < m) {
-            merge[k] = nums1[i];
-            k++;
-            i++;
-        }
-        while (j < n) {
-            merge[k] = nums2[j];
-            k++;
-            j++;
-        }
-        if ((m+n) % 2 == 0) {
-            double ans = (merge[(m+n)/2] + merge[(m+n)/2 - 1]) / 2.0000;
-            return ans;
-        } else {
-            return merge[(m+n) / 2];
-        }
+        return 0.0;
     }
 }
