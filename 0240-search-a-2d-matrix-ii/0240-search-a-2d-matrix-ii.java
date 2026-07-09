ab0d5ef70bbe66d943fd06c67d2c77fe9f41278c
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-07-08 17:28:25 -0700

    3 ms (89.25%) 0240-search-a-2d-matrix-ii Medium .java

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int m = matrix.length, n = matrix[0].length;
+
+        int i = 0, j = n - 1;
+
+        while (i < m && j >= 0) {
+            int curr = matrix[i][j];
+
+            if (curr == target) {
+                return true;
+            }
+
+            if (curr < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return false;
+    }
+}
