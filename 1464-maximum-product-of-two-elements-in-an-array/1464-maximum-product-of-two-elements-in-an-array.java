fc60db053e1dd11b29fcc6a0616eb54a41e1d119
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-07-27 14:01:19 -0700

    1 ms (66.15%) 1464-maximum-product-of-two-elements-in-an-array Easy .java

---
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+        
+        int max1 = 0;
+        int max2 = 0;
+        
+        for (int n : nums) {
+            if (max1 < n) {
+                max2 = max1;
+                max1 = n;
+            } else {
+                max2 = Math.max(max2, n);
+            }
+        }
+
+        return (max1 - 1) * (max2 - 1);
+    }
+}
