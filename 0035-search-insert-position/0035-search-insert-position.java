c4c7c51c8ab360603f1e5327b10cf27eb0f39d8e
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-07-28 17:05:58 -0700

    0 ms (100%) 0035-search-insert-position Easy .java

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,21 +1,36 @@
 class Solution {
-    public int searchInsert(int[] nums, int target) {
-        int low = 0, high = nums.length - 1;
+    public int searchInsert(int[] array, int target) {
+        // int low = 0, high = nums.length - 1;
 
-        while (low <= high) {
-            int mid = (low + high) / 2;
+        // while (low <= high) {
+        //     int mid = low + (high - low) / 2;
 
-            if (nums[mid] == target) {
+        //     if (nums[mid] == target) {
+        //         return mid;
+        //     }
+
+        //     if (nums[mid] > target) {
+        //         high = mid - 1;
+        //     } else {
+        //         low = mid + 1;
+        //     }
+        // }
+
+        // return high + 1;
+
+        int start = 0;
+        int end = array.length - 1;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (array[mid] > target) {
+                end = mid - 1;
+            } else if (array[mid] < target) {
+                start = mid + 1;
+            } else if (array[mid] == target) {
                 return mid;
             }
-
-            if (nums[mid] > target) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
         }
+        return start;
 
-        return high + 1;
     }
 }
