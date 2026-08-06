c17722346f2f35dc971c601c3a1f3b9fa5f09b4d
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-08-05 19:45:19 -0700

    1 ms (100%) 3345-smallest-divisible-digit-product-i Easy .java

---
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    private boolean check(int n, int t) {
+        
+        int prod = 1;
+
+        while (n > 0) {
+            prod *= (n % 10);
+            n /= 10;
+
+            if (prod == 0) {
+                break;
+            }
+        }
+
+        return prod % t == 0;
+    }
+
+    public int smallestNumber(int n, int t) {
+        
+        while (!check(n, t)) {
+            n++;
+        }
+
+        return n;
+    }
+}
