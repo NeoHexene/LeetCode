00c70fa218848ac13a6f713910d06f5d29a7c8a8
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-08-13 18:30:15 -0700

    22 ms (1.66%) 0216-combination-sum-iii Medium .java

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.java
@@ -0,0 +1,32 @@
+class Solution {
+
+    private void helper(int num, int k, int sum, List<Integer> temp, List<List<Integer>> res) {
+
+        System.out.println("Num: " + num + " Sum: " + sum);
+        if (sum == 0 && temp.size() == k) {
+            res.add(new ArrayList<>(temp));
+            return;
+        }
+        if (num == 10 || sum < 0 || temp.size() == k) {
+            return;
+        }
+
+        temp.add(num);
+        helper(num + 1, k, sum - num, temp, res);
+        temp.remove(temp.size() - 1);
+        helper(num + 1, k, sum, temp, res);
+    }
+
+    public List<List<Integer>> combinationSum3(int k, int n) {
+
+        if (n == 1 || n > 45) {
+            return new ArrayList<>();
+        }
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        helper(1, k, n, new ArrayList<>(), res);
+
+        return res;
+    }
+}
