c11769302f3e5cb3d795e444d943e7beb9fe5d5a
Aniruddha Khandekar <98579717+NeoHexene@users.noreply.github.com>
2026-09-06 17:49:11 -0700

    9 ms (97.96%) 1358-number-of-substrings-containing-all-three-characters Medium .java

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.java
@@ -1,16 +1,28 @@
 class Solution {
+
     public int numberOfSubstrings(String s) {
-        int[] charCount = new int[3];
-        int left = 0;
-        int res = 0;
-        for (int right = 0; right < s.length(); right++) {
-            charCount[s.charAt(right) - 'a'] += 1;
-            while (charCount[0] > 0 && charCount[1] > 0 && charCount[2] > 0) {
-                res += s.length() - right;
-                charCount[s.charAt(left) - 'a'] -= 1;
-                left++;
+        
+        int a = -1, b = -1, c = -1, count = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            char ch = s.charAt(i);
+
+            if (ch == 'a') {
+                a = i;
+            } else if (ch == 'b') {
+                b = i;
+            } else {
+                c = i;
+            }
+
+            if (a != -1 && b != -1 && c != -1) {
+                int lastidx = a < b && a < c ? a : b < c ? b : c;
+                count += (lastidx + 1);
             }
         }
-        return res;
+        
+        return count;
+
     }
 }
